bb716fdb52c0c316e948dd72fe98be2259e10118
czoido <mrgalleta@gmail.com>
2020-12-15 11:53:00 +0100

    wip

--- src/org/jfrog/conanci/TestRunner.groovy
+++ src/org/jfrog/conanci/TestRunner.groovy
@@ -197,7 +197,7 @@ class TestRunner {
                         try {
 
                             script.withEnv(["CONAN_TEST_FOLDER=${workdir}"]) {
-                                checkPytestResults(script.bat(script: "python python_runner/runner.py ${this.testModule} ${pyver} ${sourcedir} \"${workdir}\" ${numcores} ${flavor_cmd} ${eTags}", returnStatus: true)))
+                                checkPytestResults(script.bat(script: "python python_runner/runner.py ${this.testModule} ${pyver} ${sourcedir} \"${workdir}\" ${numcores} ${flavor_cmd} ${eTags}", returnStatus: true))
                             }
                         }
                         finally {
@@ -207,7 +207,7 @@ class TestRunner {
                     } else if (slaveLabel == "Macos") {
                         try {
                             script.withEnv(['PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin']) {
-                                checkPytestResults(script.sh(script: "python python_runner/runner.py ${this.testModule} ${pyver} ${sourcedir} ${workdir} ${numcores} ${flavor_cmd} ${eTags}", returnStatus: true)))
+                                checkPytestResults(script.sh(script: "python python_runner/runner.py ${this.testModule} ${pyver} ${sourcedir} ${workdir} ${numcores} ${flavor_cmd} ${eTags}", returnStatus: true))
                             }
                         }
                         finally {
@@ -222,7 +222,7 @@ class TestRunner {
                                 script.sh(script: "mkdir -p ${sourcedir}")
                                 script.sh(script: "cp -R ./ ${sourcedir}")
                                 script.sh(script: "chown -R conan ${sourcedir}")
-                                checkPytestResults(script.sh(script: "su - conan -c \"python ${sourcedir}/python_runner/runner.py ${this.testModule} ${pyver} ${sourcedir} /tmp ${numcores} ${flavor_cmd} ${eTags}\"", returnStatus: true)))
+                                checkPytestResults(script.sh(script: "su - conan -c \"python ${sourcedir}/python_runner/runner.py ${this.testModule} ${pyver} ${sourcedir} /tmp ${numcores} ${flavor_cmd} ${eTags}\"", returnStatus: true))
                             }
                         }
                         finally {
