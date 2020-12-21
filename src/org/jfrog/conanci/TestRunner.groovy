b13dc147d5bb5ebcee70dc72ccd215c48b4bccc6
czoido <mrgalleta@gmail.com>
2020-12-21 15:47:24 +0100

    wip

--- src/org/jfrog/conanci/TestRunner.groovy
+++ src/org/jfrog/conanci/TestRunner.groovy
@@ -195,8 +195,8 @@ class TestRunner {
 
                             script.withEnv(["CONAN_TEST_FOLDER=${workdir}"]) {
                                 script.bat(script: "python python_runner/runner.py ${testModule} ${pyver} ${sourcedir} \"${workdir}\" ${numcores} ${flavor_cmd} ${eTags}")
-                                archiveArtifacts artifacts: '*.xml'
-                                echo "Inspect generated webpage at ${BUILD_URL}artifact/"
+                                script.archiveArtifacts artifacts: '*.xml'
+                                script.echo "Inspect generated webpage at ${BUILD_URL}artifact/"
                             }
                         }
                         finally {
@@ -207,8 +207,8 @@ class TestRunner {
                         try {
                             script.withEnv(['PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin']) {
                                 script.sh(script: "python python_runner/runner.py ${testModule} ${pyver} ${sourcedir} ${workdir} ${numcores} ${flavor_cmd} ${eTags}")
-                                archiveArtifacts artifacts: '*.xml'
-                                echo "Inspect generated webpage at ${BUILD_URL}artifact/"
+                                script.archiveArtifacts artifacts: '*.xml'
+                                script.echo "Inspect generated webpage at ${BUILD_URL}artifact/"
                             }
                         }
                         finally {
@@ -224,8 +224,8 @@ class TestRunner {
                                 script.sh(script: "cp -R ./ ${sourcedir}")
                                 script.sh(script: "chown -R conan ${sourcedir}")
                                 script.sh(script: "su - conan -c \"python ${sourcedir}/python_runner/runner.py ${testModule} ${pyver} ${sourcedir} /tmp ${numcores} ${flavor_cmd} ${eTags}\"")
-                                archiveArtifacts artifacts: '*.xml'
-                                echo "Inspect generated webpage at ${BUILD_URL}artifact/"
+                                script.archiveArtifacts artifacts: '*.xml'
+                                script.echo "Inspect generated webpage at ${BUILD_URL}artifact/"
                             }
                         }
                         finally {
