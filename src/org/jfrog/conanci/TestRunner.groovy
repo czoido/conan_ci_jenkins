b7c5739a639f77957c06d3c0979dac3415b040df
czoido <mrgalleta@gmail.com>
2020-12-21 16:08:11 +0100

    wip

--- src/org/jfrog/conanci/TestRunner.groovy
+++ src/org/jfrog/conanci/TestRunner.groovy
@@ -224,6 +224,9 @@ class TestRunner {
                                 script.sh(script: "cp -R ./ ${sourcedir}")
                                 script.sh(script: "chown -R conan ${sourcedir}")
                                 script.sh(script: "su - conan -c \"python ${sourcedir}/python_runner/runner.py ${testModule} ${pyver} ${sourcedir} /tmp ${numcores} ${flavor_cmd} ${eTags}\"")
+                                script.sh(script: "ls ${sourcedir}")
+                                script.sh(script: "ls")
+                                script.sh(script: "find . -name nose\*")
                                 script.archiveArtifacts artifacts: "${sourcedir}/**/*.xml"
                                 script.echo "Inspect generated webpage at ${BUILD_URL}artifact/"
                             }
